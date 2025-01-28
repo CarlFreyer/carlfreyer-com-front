838601aaacb380c98110c5dd2e3541daae1ba254
Carl Freyer <ckf225@lehigh.edu>
2025-01-28 16:21:56 -0500

    test

--- src/app/routes/home.jsx
+++ src/app/routes/home.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import '../styles/home.css'
-import LinkedInLogo from '../../assets/linkedin.svg?react'
-import RoadClosedLogo from '../../assets/roadClosed.svg?react'
-import CraneLogo from '../../assets/crane.svg?react'
-import DigLogo from '../../assets/dig.svg?react'
 
 let Home = () => {
     return (
@@ -12,9 +8,6 @@ let Home = () => {
                 <div>
                     <div className='fullWidthContainer'>
                         <div className='multiLogoContainer'>
-                            <DigLogo alt="A man digging."/>
-                            <CraneLogo alt="A construction crane."/>
-                            <RoadClosedLogo alt="A road closed barrier."/>
                         </div>
                     </div>
                     <h1>Hello and welcome!</h1>
@@ -22,7 +15,6 @@ let Home = () => {
                     <div className="fullWidthContainer">
                         <a href="https://www.linkedin.com/in/carl-k-freyer/" className="hiddenLink leftImageRightText linkedInColors">
                             <div>
-                                <LinkedInLogo className="smallLogo" alt="LinkedIn Logo"/>
                             </div>
                             <div>
                                 <p>
