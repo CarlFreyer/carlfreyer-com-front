b6c5d178c4230c49487b03c0cadcc5e2d8e25892
Carl Freyer <ckf225@lehigh.edu>
2025-01-28 16:59:25 -0500

    better for mobile

--- src/app/routes/home.jsx
+++ src/app/routes/home.jsx
@@ -9,28 +9,28 @@ let Home = () => {
     return (
         <div className="standard_page_container">
             <div className="main_middle">
-                <div>
-                    <div className='fullWidthContainer'>
-                        <div className='multiLogoContainer'>
-                            <DigLogo alt="A man digging."/>
-                            <CraneLogo alt="A construction crane."/>
-                            <RoadClosedLogo alt="A road closed barrier."/>
-                        </div>
+                <div className='fullWidthContainer'>
+                    <div className='multiLogoContainer'>
+                        <DigLogo alt="A man digging."/>
+                        <CraneLogo alt="A construction crane."/>
+                        <RoadClosedLogo alt="A road closed barrier."/>
                     </div>
+                </div>
+                <div className='fullWidthContainer'>
                     <h1>Hello and welcome!</h1>
                     <h2>This website is undergoing some construction right now. Please watch your step!</h2>
-                    <div className="fullWidthContainer">
-                        <a href="https://www.linkedin.com/in/carl-k-freyer/" className="hiddenLink leftImageRightText linkedInColors">
-                            <div>
-                                <LinkedInLogo className="smallLogo" alt="LinkedIn Logo"/>
-                            </div>
-                            <div>
-                                <p>
-                                    In the meantime, click here follow me on LinkedIn!
-                                </p>
-                            </div>
-                        </a>
-                    </div>
+                </div>
+                <div className="fullWidthContainer">
+                    <a href="https://www.linkedin.com/in/carl-k-freyer/" className="hiddenLink leftImageRightText linkedInColors">
+                        <div>
+                            <LinkedInLogo className="smallLogo" alt="LinkedIn Logo"/>
+                        </div>
+                        <div>
+                            <p>
+                                In the meantime, click here follow me on LinkedIn!
+                            </p>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
