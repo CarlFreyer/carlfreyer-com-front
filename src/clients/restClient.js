e88b4f64b27fb7772e22bcbf17ec832a7fe8ac5b
Carl Freyer <ckf225@lehigh.edu>
2025-01-30 13:52:30 -0500

    remove catching

--- src/clients/restClient.js
+++ src/clients/restClient.js
@@ -1,13 +1,13 @@
 const fetchRequest = async (params) => {
-    try{
+    //try{
         const response = await fetch(params);
         if(response.ok){
             const responseJson = await response.json()
             return {status: response.status, statusText: response.statusText, json: responseJson}
         }else return {status: response.status, statusText: response.statusText}
-    }catch{
-        return {status: 500, statusText: "Internal Server Error"}
-    }
+    //}catch{
+    //    return {status: 500, statusText: "Internal Server Error"}
+    //}
 }
 
 export default {
